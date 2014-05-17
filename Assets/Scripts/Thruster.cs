496120bc0418d311498ac203512f8ff535f3c194
Andrew O'Connor <bravesirandrew@gmail.com>
2014-05-17 17:45:07 +0100

    Thrusters now work with different gamepad sticks and change particle lifetimes.

--- Assets/Scripts/Thruster.cs
+++ Assets/Scripts/Thruster.cs
@@ -1,26 +1,24 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Thruster : MonoBehaviour 
 {
-	public float _maxThrust;
+	public float MaxThrust;
+	public string HorizontalAxisName;
+	public string VerticalAxisName;
+	public float MaxParticleLifetime = 0.21f;
 
-	// Use this for initialization
 	void Start () 
 	{
 	
 	} 
 	
-	// Update is called once per frame
 	void Update ()
 	{
-		transform.LookAt(transform.position + new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical") * -1));
+		var stickDirection = new Vector3 (Input.GetAxis (HorizontalAxisName), 0, Input.GetAxis (VerticalAxisName) * -1);
+		transform.LookAt(transform.position + stickDirection);
 		gameObject.transform.parent.rigidbody.AddForceAtPosition(
-			new Vector3 (Input.GetAxis ("Horizontal") * -1 * _maxThrust, 0, Input.GetAxis ("Vertical") * _maxThrust), 
+			new Vector3(Input.GetAxis(HorizontalAxisName) * -1 * MaxThrust, 0, Input.GetAxis(VerticalAxisName) * MaxThrust), 
 			transform.position);
-
-
+		particleSystem.startLifetime = MaxParticleLifetime * stickDirection.magnitude;
 	}
-
-
 }
