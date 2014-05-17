eda8c58610f27dee03e301422a6e2c23f2221a15
Andrew O'Connor <bravesirandrew@gmail.com>
2014-05-17 16:51:05 +0100

    Second commit:)

---
+++ Assets/Editor/FixStupidEditorBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+public class FixStupidEditorBehavior : MonoBehaviour
+{
+	[MenuItem("GameObject/Create Empty Child #&n")]
+	static void BringMeBackFromTheEdgeOfMadness()
+	{
+		GameObject go = new GameObject("GameObject");
+
+		if (Selection.activeTransform != null)
+		{
+			go.transform.parent = Selection.activeTransform;
+			go.transform.position = Vector3.zero;
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Thruster.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thruster : MonoBehaviour 
+{
+	public float _maxThrust;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	} 
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.LookAt(transform.position + new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical") * -1));
+		gameObject.transform.parent.rigidbody.AddForceAtPosition(
+			new Vector3 (Input.GetAxis ("Horizontal") * -1 * _maxThrust, 0, Input.GetAxis ("Vertical") * _maxThrust), 
+			transform.position);
+
+
+	}
+
+
+}
