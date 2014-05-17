fecf42451691d26c2b24baef4db502c1aa741456
Michael <ml_o_kane@hotmail.com>
2014-05-17 22:32:06 +0100

    Boost pads

---
+++ Assets/Scripts/BoostPad.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class BoostPad : MonoBehaviour 
+{
+	public float Force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var thrusters = GameObject.FindGameObjectsWithTag ("Thruster");
+		var direction = transform.TransformDirection(new Vector3 (0, 0, 1));
+
+		foreach (var thruster in thrusters) 
+		{
+			if(!PadContainsObject(thruster.transform.position))
+			   continue;
+			  
+			thruster.transform.parent.rigidbody.AddForceAtPosition(direction * Force, thruster.transform.position);
+		}
+	}
+
+	private bool PadContainsObject(Vector3 position)
+	{
+		return collider.bounds.Contains(position);
+	}
+}
